72a5e4faa9cdd9d789de6ee18fbaa8318b8eb072
thisLucio <luxusnietzsche@gmail.com>
2022-09-10 14:02:43 -0300

    Criação TopicoDto

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,20 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicosController {
+
+    @RequestMapping("/topicos")
+    public List<TopicoDto> lista(){
+        Topico topico = new Topico("Dúvida", "Duvida com Spring", new Curso("Spring", "Programacao"));
+        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    }
+}
